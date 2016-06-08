4ba5e78478d786ad399c3a5560998a746b8e1618
teazmo <timo.ihnken@googlemail.com>
2016-06-08 23:33:59 +0200

    Update helloworld.php

--- admin/helloworld.php
+++ admin/helloworld.php
@@ -1,8 +1,11 @@
 <?php
 
-defined('_JEXEC') or die();
+defined('_JEXEC') or die;
 
+$controller = JControllerLegacy::getInstance('helloworld');
 
+$input = JFactory::getApplication()->input;
 
+$controller->execute($input->getCmd('task'));
 
-?>
+$controller->redirect();
